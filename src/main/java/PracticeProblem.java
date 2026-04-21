7a7cfd065e0781c3a4f192d06ec5f12ea8a43391
Nithin Alagar <343954178@tdsb.ca>
2026-04-21 10:51:32 -0400

    Nithin.A

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -5,5 +5,51 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
+		public static int intChecker(){
+	    Scanner s = new Scanner(System.in);
+	    do{
+	        s.nextLine();
+	        System.out.print("Input an integer: ");
+	        
+	    } while (!s.hasNextInt());
+	    return s.nextInt();
+	    
+	}
+	
+	public static double doubleChecker(){
+	    Scanner s = new Scanner(System.in);
+	    do{
+	        s.nextLine();
+	        System.out.print("Input a number: ");
+	        
+	    } while (!s.hasNextDouble());
+	    return s.nextDouble();
+	    
+	}
+	
+	public static boolean booleanChecker(){
+	    Scanner s = new Scanner(System.in);
+	    do{
+	        s.nextLine();
+	        System.out.print("Input a boolean: ");
+	        
+	    } while (!s.hasNextBoolean());
+	    return s.nextBoolean();
+	    
+	}
+	
+	public static boolean multipleIntChecker(){
+	    Scanner s = new Scanner(System.in);
+	    for (int i = 0; i<4; i++){
+	    do{
+	        s.nextLine();
+	        System.out.print("Input an integer: ");
+	        
+	    } while (!s.hasNextInt());
+	        
+	    }
+	    
+	    return true;
+	}
 
 }
